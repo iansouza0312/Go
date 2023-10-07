b4dc226169dadd8cade374e56579460f5a971817
Ian Souza <ianfmsouza@gmail.com>
2023-10-07 15:49:50 -0300

    Refatored adress_test function with multiple usecases

--- AutomatedTests/adressTest/adress/adress_test.go
+++ AutomatedTests/adressTest/adress/adress_test.go
@@ -2,21 +2,27 @@ package adress
 
 import "testing"
 
+type testingCase struct {
+	recievedAdress string
+	expectedReturn string
+}
+
 func TestAdressType(t *testing.T) {
-	adressForSuccess := "Avenida dos Asstronautas"
-	adressForError := "Rodovia dos Tamoios"
 
-	expectedAdressType := "Avenida"
-
-	recievedAdressType := AdressType(adressForSuccess)
-	recievedAdressTypeError := AdressType(adressForError)
-
-	if recievedAdressType != expectedAdressType{
-		t.Errorf("Tipo recebido e diferente do esperado, esperava %s e recebeu %s", expectedAdressType, recievedAdressType)
+	testingCases := []testingCase{
+		{  "Rua dos passaros", "Rua" },
+		{  "Avenida dos Astronautas", "Avenida" },
+		{  "Rodovia dos Tamoios", "Rodovia" },
+		{  "Rodovia Presidente Dutra", "Rodovia" },
+		{  "Serra do mar", "Serra" },
+		{  "Rua 12 de março", "Rua" },
+		{  "Curva do S ", "Tipo de endereço invalido." },
 	}
 
-	// Error case
-	if recievedAdressType != recievedAdressTypeError{
-		t.Errorf("Tipo recebido e diferente do esperado, esperava %s e recebeu %s", expectedAdressType, recievedAdressTypeError)
+	for _, cenario := range testingCases {
+		adressType := AdressType(cenario.recievedAdress)
+		if adressType != cenario.expectedReturn {
+			t.Errorf("O tipo recebido %s e diferente do esperado %s", adressType, cenario.expectedReturn)
+		}
 	}
 }
